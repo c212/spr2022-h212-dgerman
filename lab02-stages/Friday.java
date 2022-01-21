cd16cfe833b18571eb4e000346dd335f0dd91391
c212 <40395648+c212@users.noreply.github.com>
2022-01-21 16:27:55 -0500

    our first paintComponent

--- lab02-stages/Friday.java
+++ lab02-stages/Friday.java
@@ -1,5 +1,6 @@
 import javax.swing.JFrame;
 import javax.swing.JComponent;
+import java.awt.Graphics;
 
 public class Friday extends JComponent {
     public static void main(String[] args) {
@@ -7,6 +8,9 @@ public class Friday extends JComponent {
         // System.out.println( a );
         a.setSize(400, 400);
         a.setVisible(true);
+
+        Friday e = new Friday();
+        a.add(e); 
         
         Horse b = new Horse(); 
         Unicorn c = new Unicorn(); 
@@ -15,5 +19,9 @@ public class Friday extends JComponent {
         b.talk(); // Howdy
         c.talk(); // Bonjour
         d.talk(); // Bonjour
+    }
+    int i = 5;
+    public void paintComponent(Graphics g) {
+        System.out.println("Bonjour... " + ++i);         
     }
 }
