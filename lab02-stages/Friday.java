231cb5b0abdf0c0cf73d974074c0dfa4479862b5
c212 <40395648+c212@users.noreply.github.com>
2022-01-21 15:47:12 -0500

    defined a penguin

--- lab02-stages/Friday.java
+++ lab02-stages/Friday.java
@@ -1,3 +1,10 @@
+import javax.swing.JFrame;
+
 public class Friday {
-    
+    public static void main(String[] args) {
+        JFrame a = new JFrame("Move circles with the mouse.");
+        // System.out.println( a );
+        a.setSize(400, 400);
+        a.setVisible(true);
+    }
 }
---
+++ lab02-stages/Penguin.java
@@ -0,0 +1,13 @@
+import java.awt.Graphics;
+
+public class Penguin { // actually a circle 
+    int x, y, radius; 
+    public Penguin(int x, int y, int radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    public void ward(Graphics a) {
+        
+    }
+}
