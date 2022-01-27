689958bd726efb84ffafe720b9cecef0f6d8b08d
c212 <40395648+c212@users.noreply.github.com>
2022-01-27 14:33:20 -0500

    what with arrows

--- lectures/01-27-thu/What.java
+++ lectures/01-27-thu/What.java
@@ -1,42 +1,31 @@
 import java.awt.*; 
 
 public class What {
-  int x, y; // since each node needs to be drawn somewhere 
-  public void moveTo(int x, int y) { //since we intend to move it
-    this.x = x;
-    this.y = y; 
+  final int radius = 20; 
+  int value; 
+  What next;
+  int x, y;
+  boolean contains(int x, int y) {
+    return radius >= Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2));  
   }
-  int value; //every node is meant to carry a value
-  What next; //this is the linked nature of What
-  What(int value, What next) { // the constructor
+  boolean drawn = false; 
+  public What(int value, What next) {
     this.value = value; 
     this.next = next; 
-    this.x = (int)(Math.random() * 400 + 50); // coordinates are initially random 
+    this.x = (int)(Math.random() * 400 + 50); 
     this.y = (int)(Math.random() * 400 + 50); 
-    
   }
-  public boolean contains(int x, int y) { // we need to know when we have clicked inside with the mouse 
-    return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2)) <= 20;  
+  public void draw(Graphics g) { 
+    g.setColor(Color.WHITE); 
+    g.fillOval(this.x, this.y, 2 * radius, 2 * radius); 
+    g.setColor(Color.BLACK); 
+    g.drawOval(this.x, this.y, 2 * radius, 2 * radius); 
+    Font myFont = new Font("Courier", Font.BOLD, 20); // numbers inside nodes have a certain size  
+    ((Graphics2D)g).setFont(myFont);
+    ((Graphics2D)g).drawString(this.value + "", this.x + radius*2/3, this.y + radius*4/3); 
   }
-  public void draw(Graphics g) { // this is the recursive draw function  
-    Graphics2D g2 = (Graphics2D)g;
-    float thickness = 6;
-    Stroke oldStroke = g2.getStroke();
-    g2.setStroke(new BasicStroke(thickness)); // lines between nodes have a certain thickness  
-    Font myFont = new Font("Courier", Font.BOLD, 40); // numbers inside nodes have a certain size  
-    g2.setFont(myFont); 
-    if (this.next == null) {
-      // see below 
-    } else {
-      g2.drawLine(this.x, this.y, this.next.x, this.next.y); 
-      this.next.draw(g); 
-    }
-    // here     
-    g2.setColor(Color.YELLOW); // draw the inside  
-    g2.fillOval(this.x - 30, this.y - 30, 60, 60); 
-    g2.setColor(Color.BLACK); // draw the outside  
-    g2.drawOval(this.x - 30, this.y - 30, 60, 60); 
-    g2.drawString(this.value + "", this.x-10, this.y+10); // and the value carried 
-
+  void moveTo(int x, int y) {
+    this.x = x;
+    this.y = y; 
   }
 }
