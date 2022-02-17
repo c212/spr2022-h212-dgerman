1d4128b31330390b32dd3ebf4701ebcd6546d8c9
c212 <40395648+c212@users.noreply.github.com>
2022-02-17 14:26:06 -0500

    second method

--- lectures/02-15-tue/Line.java
+++ lectures/02-15-tue/Line.java
@@ -10,7 +10,12 @@ public class Line {
         this.a.draw(g); 
         // g.drawLine(this.a.x, this.a.y, this.b.x, this.b.y);
         int x1 = a.x, y1 = a.y, x2 = b.x, y2 = b.y;
-        this.drawLine(x1, y1, x2, y2, g);
+        // this.drawLine(x1, y1, x2, y2, g);
+        for (double t = 0; t < 1; t += 0.1) { // shane's method
+            int x = (int) (Math.round(x1 + t * (x2 - x1)));
+            int y = (int) (Math.round(y1 + t * (y2 - y1))); 
+            g.drawOval(x - 5, y - 5, 10, 10); 
+        }
         this.b.draw(g);
     }
     public void drawLine(int x1, int y1, int x2, int y2, Graphics g) {
